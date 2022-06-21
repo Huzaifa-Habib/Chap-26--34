e9fa9afce13cae75881ecfd1f49e279586a6e266
Huzaifa Habib <101453901+cocobutte@users.noreply.github.com>
2022-06-21 21:38:06 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,69 @@
+
+var num = parseFloat(prompt("Enter a positive number "));
+
+document.getElementById("num").innerText = num; 
+
+var round = Math.round(num);
+document.getElementById("round").innerText = round; 
+
+var flor =Math.floor(num);
+document.getElementById("floor").innerText = flor; 
+
+var ciel = Math.ceil(num);
+document.getElementById("ciel").innerText = ciel; 
+
+
+// Question 2===============================================================================
+
+var mnum = parseFloat(prompt("Enter a negative number "));
+
+document.getElementById("mnum").innerText = mnum; 
+
+var round = Math.round(mnum);
+document.getElementById("mround").innerText = round; 
+
+var flor =Math.floor(mnum);
+document.getElementById("mfloor").innerText = flor; 
+
+var ciel = Math.ceil(mnum);
+document.getElementById("mciel").innerText = ciel; 
+
+// Question 3===============================================================================
+var pnum = parseFloat(prompt("Enter a positive floating-point number "));
+
+document.getElementById("pnum").innerText = pnum; 
+
+var round = Math.round(pnum);
+document.getElementById("pround").innerText = round; 
+
+var flor =Math.floor(pnum);
+document.getElementById("pfloor").innerText = flor; 
+
+var ciel = Math.ceil(pnum);
+document.getElementById("pciel").innerText = ciel; 
+
+// Question 4===============================================================================
+var mpnum = parseFloat(prompt("Enter a negative floating-point number "));
+
+document.getElementById("mpnum").innerText = mpnum; 
+
+var round = Math.round(mpnum);
+document.getElementById("mpround").innerText = round; 
+
+var flor =Math.floor(mpnum);
+document.getElementById("mpfloor").innerText = flor; 
+
+var ciel = Math.ceil(mpnum);
+document.getElementById("mpciel").innerText = ciel; 
+
+
+
+
+
+
+
+
+
+
+
+
